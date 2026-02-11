e7e8c20b7aa0089e4d2eb427a2f59df46d3add7e
te-dino <dyaltiwar@techechelons.com>
2026-02-11 15:32:40 +0530

    Implement Eslint and Prettier

--- src/app.js
+++ src/app.js
@@ -11,12 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
 
 // 404 Handler
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).json({ error: 'Not Found' });
 });
 
 // Error Handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal Server Error' });
 });
