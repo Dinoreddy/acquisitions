0407eaef9f95fdf7414b10aad2c7f4dcfa0d0f91
te-dino <dyaltiwar@techechelons.com>
2026-02-11 15:12:53 +0530

    Initial commit w/ basic setup

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import routes from './routes/index.js';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api', routes);
+
+// 404 Handler
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+export default app;
